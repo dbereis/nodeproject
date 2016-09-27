83364991c7c23c91930b035f02eac2bbe8f2df0e
Daniel <daltoniko@gmail.com>
2016-09-27 01:26:46 +0000

    Adding bootstrap template

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,19 @@
-var aloha = 'Aloha variable';
-console.log(aloha);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req,res){
+    res.send('Aloha world!');
+});
+
+app.get('/routing', function(req,res){
+    res.send('Aloha routing!');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port:' + port);
+});
